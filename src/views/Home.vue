a48a068f85540d8447746eafd083d6b9404e5818
robit <36677806+robit-man@users.noreply.github.com>
2022-04-01 02:00:46 +0000

    updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
   <div class="cornerbox" style="padding-top:2rem;padding-right:2rem;margin-left:auto;">
       <div class="button">CONNECT WALLET</div>
 </div>
-<jj-parallax-card class="parallaxtop" lightColor="transparent" style="margin:auto;margin-top:1rem; width: 100%; max-width:calc(100vw - 2rem;);min-height:140px;" @click="click">
+<jj-parallax-card class="parallaxtop" lightColor="transparent" style="margin:auto;margin-top:auto; margin-bottom:2rem;width: 100%; max-width:calc(100vw - 2rem;);min-height:140px;" @click="click">
       <div slot="front" style="">
       <h1 class="parallaxtoph1">CHICKEN GLADIATORS</h1>
       <h2 style="text-align:center;font-size:1rem;">10,000 Unique Generative NFT's</h2>
@@ -24,17 +24,20 @@
 
     <div class="whitelist" style="width:512px;min-width:512px;"> 
     
-     <h1 style="font-size:1.5rem;">OG Whitelist: OPEN NOW!</h1>
+     <h1 style="font-size:2.5rem;text-shadow:2px 2px black;">OG Whitelist: <b style="color:indianred;">OPEN NOW!</b></h1>
      <br>   
 
-     <div class="button"><h2>JOIN WHITELIST</h2></div>
+     <div class="button joinbutton" >JOIN WHITELIST</div>
     <br>
      <br>
      <h2>CLOSES IN: <br>
         <Countdown deadline="April 1, 2022"></Countdown>
     </h2>
         <br>
-
+<p>
+    Watch our social for further updates... 
+    <br><br></p>
+    
     <div style="display:flex;margin:auto;flex-flow:row;justify-content:center;">
     <a target="_blank" href="https://twitter.com/ChickenGladsNFT "><img style="width:64px;" src="~/@/assets/twitter.svg" alt=""></a>
     <a target="_blank" href="https://discord.gg/d9MqkSG6 "><img style="width:64px;" src="~/@/assets/discord.svg" alt=""></a>
@@ -43,10 +46,7 @@
         <br>
 
     <h2>More To Come!</h2>
-    <p>
-    Watch our social for further updates... 
-    <br><br>
-    </p>
+  <br>
     <ul>
     <li>Contests</li>
     <li>Roadmap</li>
@@ -106,6 +106,8 @@ h2{font-size:1.5rem;}
   .gladiators > img:hover{transform:scale(1.1);}
 .whitelist{text-align:center;}
 .parallaxtop{}
+.joinbutton{background-color:gold;border:unset;font-size:1.5rem;box-shadow:0px 2px black;}
+.connectwallet{background-color:transparent;border:1px solid white;font-size:1.5rem;box-shadow:0px 2px black;color:white;}
 @media screen and (max-width:600px) {
    .gladiators{display:flex;flex-flow:row;justify-content:center;}
   .gladiators > img{margin:auto;max-height:128px;;}
