6bd4efefc4a68dadc42668c50ce851084a43f9da
robit <36677806+robit-man@users.noreply.github.com>
2022-05-04 04:03:47 +0000

    sexification

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
+    <div style="height:100vh;margin:unset;overflow:hidden;">
+      
+      <div class="menu">
+      <div class="row" style="max-width:112rem;margin:auto;width:100%;justify-content:space-between;">
+        <a href="/"> <div class="tomatobutton">TOMATOVERSE</div></a>
+
+        <div class="linkrow">
+
+        <a href="" class="menubutton" style="border:1px solid #1115;">
+          MINT
+        </a>
+        <a href="" class="menubutton">
+          DUELS
+        </a>
+        <a href="" class="menubutton">
+          TROOPERS
+        </a>
+        
+        </div>
+        </div>
+      </div>
+
     <div id="app" class="" style="">
       <VueLoadingIndicator v-if="settings.loading" class="overlay big" />
       <div v-else>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,168 +1,57 @@
 <template>
   <div class="full-wrapper">
-<div class="hero" style="min-height:50vh;width:100vw;background-color:#1e110d;display:flex;flex-flow:column;background-attachment:fixed;justify-content:center;background-blend-mode:soft-light;background-image:url(https://jooinn.com/images/dirt-texture-43.jpg);">
-  <div class="hero-centered" style="margin:auto;text-align:center;">
-   <h1 style="font-size:7rem;">Tomato Verse</h1>
-   <br>
-   <p>He who controls the Sauce Controls the TomatoVerse</p>
-   <br>
-   <h3>COMING SOON...</h3>
-  </div>
-</div>
 
-
-<div class="row-wrapper" style="background-color:#b6b0e6;padding:2rem;">
-  <div class="row" style="display:flex;flex-flow:row;justify-content:center;">
+  <div class="hero" style="overflow:hidden;
+    backdrop-filter: blur(7px);height:100vh;width:100vw;z-index:50;display:flex;flex-flow:column;justify-content:center;">     
   
-    <div class="charbox" style="margin:0rem 1rem;">
-      <img src="~/@/assets/tomato/Final2.png" style="width:400px;max-width:100%" alt="">
-      <div class="desc" style="text-shadow:unset!important;color:black;">
-      <br>
-  <h3 style="text-shadow:unset!important;">Beefstake Bros</h3>
-  <br>
-<div class="row" style="justify-content:space-between;">
-  <div class="chartitle">  
-  <p>Total Population</p>
-  <p>Trooper Population</p>
-  <p>Faction Power</p>
-  <p>Faction Prosperity</p>
-</div>
-  <div class="charval">
-  <p>1500</p>
-  <p>0</p>
-  <p>0</p>
-  <p>0</p></div>
-</div>
+  <img src="~/@/assets/tomato/tomato.png" style="
+    height: max-content;width:auto;
+    max-width:100rem;z-index:10;margin:0rem  auto;
+    max-height:30vh;
+    image-rendering: -webkit-optimize-contrast;" alt="">
+ 
+  <img src="~/@/assets/tomato/verse.png" style="
+    height: max-content;width:auto;
+   max-width:100rem;z-index:10;margin:0rem auto;max-height:30vh;
+    image-rendering: -webkit-optimize-contrast;" alt="">
 
-      </div>
-    </div>
-    
-    <div class="charbox" style="margin:0rem 1rem;">
-      <img src="~/@/assets/tomato/Final1.png" style="width:400px;max-width:100%" alt="">
-      <div class="desc" style="text-shadow:unset!important;color:black;">
-      <br>
-  <h3 style="text-shadow:unset!important;" >Cherry Cuties</h3>
-  <br>
-<div class="row" style="justify-content:space-between;">
-  <div class="chartitle">
-  <p>Total Population</p>
-  <p>Trooper Population</p>
-  <p>Faction Power</p>
-  <p>Faction Prosperity</p></div>
-  <div class="charval">
-  <p>1500</p>
-  <p>0</p>
-  <p>0</p>
-  <p>0</p></div>
-</div>
+ <img src="~/@/assets/tomato/VINES.png" style="
+    image-rendering: -webkit-optimize-contrast;width:100vh;margin:unset; transform:rotate(-90deg);transform=origin:bottom left; position: absolute;
+    bottom: 29vh;
+    left: -21vw;image-rendering: -webkit-optimize-contrast;" alt="">
+ </div>
 
-      </div>
-    </div>
-    
-    <div class="charbox" style="margin:0rem 1rem;">
-      <img src="~/@/assets/tomato/Final3.png" style="width:400px;max-width:100%" alt="">
-      <div class="desc" style="text-shadow:unset!important;color:black;">
-      <br>
-  <h3 style="text-shadow:unset!important;">Heirloom Hunks</h3>
-  <br>
-<div class="row" style="justify-content:space-between;">
-  <div class="chartitle">
-  <p>Total Population</p>
-  <p>Trooper Population</p>
-  <p>Faction Power</p>
-  <p>Faction Prosperity</p></div>
-  <div class="charval">
-  <p>1500</p>
-  <p>0</p>
-  <p>0</p>
-  <p>0</p></div>
-</div>
+  <div class="background-scroller" style="position:fixed;bottom:0px;">
+  <div class="tomatorow" style="height:100vh;width:400vw;overflow:visible;display:flex;flex-flow:row;animation:tomatoflow 30s linear infinite;opacity:0.5;">
+    <div style="width:100vw;display:flex;flex-flow:wrap;justify-content:center;"><img src="~/@/assets/tomato/1.png" style="position:relative;bottom:0px;height: 100%;" alt=""></div>
+    <div style="width:100vw;display:flex;flex-flow:wrap;justify-content:center;"><img src="~/@/assets/tomato/3.png" style="position:relative;bottom:0px;height: 100%;" alt=""></div>
+    <div style="width:100vw;display:flex;flex-flow:wrap;justify-content:center;"><img src="~/@/assets/tomato/2.png" style="position:relative;bottom:0px;height: 100%;" alt=""></div>
+    <div style="width:100vw;display:flex;flex-flow:wrap;justify-content:center;"><img src="~/@/assets/tomato/1.png" style="position:relative;bottom:0px;height: 100%;" alt=""></div>
 
-      </div>
-    </div>
+
   </div>
 
-</div>
+  </div>
 
-
-<div class="descwrap" style="padding:2rem;">
-<div class="desctitle" style="width:1080px;margin:auto;"><br><br> <h1 style=" text-align:center;font-size:5rem;color:red;">Welcome To The Tomatoverse </h1><br><br>
-<p style="color:black;">The TomatoVerse is a competitive community NFT game on the Ethereum blockchain. </p><br>
-<p style="color:black;">Each Tomato NFT will grant access to one of three factions. The three factions will compete against each other in battles, with the winning faction being rewarded with the most Sauce NFTs. Tomato holders will be able to use these Sauce NFTs in a variety of ways to benefit themselves or their faction. </p>
-<br><p style="color:black;">There are no rules in the TomatoVerse, and therefore factions will need to cooperate and self-organize to become the greatest faction.</p>
-<br><p>The goal is to use sustainable NFT inflation mechanics to allow for a unique NFT experience. 
-</p>
-
-</div>
-</div>
-
-
-
-<div class="row-wrapper" style="background-color:#b6b0e6;padding:2rem;">
-<br><br> <h3 style=" text-align:center;font-size:3rem;color:white;">The TomatoVerse NFT Ecosystem </h3><br><br><br>
-  <div class="row" style="display:flex;flex-flow:row;justify-content:center;">
+  <div class="mint-cta-box" style="display:flex;flex-flow:row;justify-content:space-between;padding:1rem;position:fixed;bottom:2rem; right:2rem;z-index:200;width:660px;height:80px;background:indianred;box-shadow:2px 5px 20px rgba(0,0,0,0.5);border-radius:0.25rem;" >
   
-    <div class="charbox" style="margin:0rem 1rem;max-width:400px;">
-      <img src="~/@/assets/tomato/Final2.png" style="width:400px;max-width:100%" alt="">
-      <div class="desc" style="text-shadow:unset!important;color:black;">
-      <br>
-  <h3 style="text-shadow:unset!important;">Tomato Villagers</h3>
-  <br>
-  <ul>
-  <li>Grants membership to factions</li>
-  <li>Eligible for faction battle Sauce rewards
-</li>
-  <li>Max 4500 supply
-</li></ul>
-
-      </div>
-    </div>
-    
-    <div class="charbox" style="margin:0rem 1rem;max-width:400px;">
-      <img src="~/@/assets/tomato/Final1.png" style="width:400px;max-width:100%" alt="">
-      <div class="desc" style="text-shadow:unset!important;color:black;">
-      <br>
-  <h3 style="text-shadow:unset!important;" >Tomato Troopers</h3>
-  <br>
-  <ul>
-  <li>Grants membership to factions</li>
-  <li>Grants the ability to participate in PvP duels
-
-</li>
-  <li>Can be minted using Sauce NFTs + a Villager
-
-</li></ul>
-
-      </div>
-    </div>
-    
-    <div class="charbox" style="margin:0rem 1rem;max-width:400px;">
-      <img src="~/@/assets/tomato/Final3.png" style="width:400px;max-width:100%" alt="">
-      <div class="desc" style="text-shadow:unset!important;color:black;">
-      <br>
-  <h3 style="text-shadow:unset!important;">The Sauce</h3>
-  <br>
-  <ul>
-  <li>Only obtained via winning faction battles
-</li>
-  <li>Burned upon use 
-
-</li>
-  <li>Can be used to mint Troopers, level a Trooper, or improve the prosperity of a faction
-
-</li></ul>
-
-      </div>
-    </div>
+  <div style="display:flex;flex-flow:row;">
+  <img src="~/@/assets/soup.svg" style="animation:spin 10s linear infinite;width:3rem;padding:0.25rem;height:3rem;background:white;border-radius:100%;" alt="">
+  <div><p style="color:white;opacity:0.5;line-height:1;font-size:0.75rem;padding-left:1rem;padding-bottom:unset;margin-bottom:0.25rem;">A NEW P2E PLATFORM</p>
+  <p style="font-weight:bold;padding-left:1rem;margin:auto;margin-top:unset;color:white;line-height:1;font-size:1rem;">GET LOST <br>IN THE SAUCE</p></div>
+  </div>
+  
+  <div class="buttoncta" style="">
+  <b style="margin:auto;
+    line-height: 1rem;">MINT SOON </b>
+  <b style="margin:auto;
+    line-height: 1rem;"><img class="arrow" src="~/@/assets/right-arrow.svg" style="width:16px;" alt=""></b>
   </div>
 
-</div>
-
-
-
+  </div>
     </div>
 </template>
-
+-
 <script>
 import { mapState, mapActions } from 'vuex';
 export default {
